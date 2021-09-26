630f3b5300d7a89b78057b04c02640503265fda8
eka <katrinmadness17@gmail.com>
2021-09-26 19:08:20 +0300

    Подключен тестовый фреймворк и добавлены тесты на расстояние

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -16,6 +16,19 @@ public class Distance {
 
     double d2 = Point.distance(p3, p4);
     System.out.println("Расстояние между точками равно" + " = " + d2);
+
+    Point p5 = new Point(-300, 10);
+    Point p6 = new Point(69, -3);
+
+    double d3 = Point.distance(p5, p6);
+    System.out.println("Расстояние между точками равно" + " = " + d3);
+
+    Point p7 = new Point(18.1, 0);
+    Point p8 = new Point(36.3, 3);
+
+    double d4 = Point.distance(p7, p8);
+    System.out.println("Расстояние между точками равно" + " = " + d4);
+
   }
 
 }
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,36 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance1() {
+    Point p1 = new Point(5, 6);
+    Point p2 = new Point(7, 8);
+    Assert.assertEquals(Point.distance(p1, p2), 2.8284271247461903);
+  }
+
+  @Test
+  public void testDistance2() {
+    Point p3 = new Point(100, 20);
+    Point p4 = new Point(75, 18);
+    Assert.assertEquals(Point.distance(p3, p4), 25.079872407968907);
+  }
+
+  @Test
+  public void testDistance3() {
+    Point p5 = new Point(-300, 10);
+    Point p6 = new Point(69, -3);
+    Assert.assertEquals(Point.distance(p5, p6), 369.2289262774519);
+  }
+
+  @Test
+  public void testDistance4() {
+    Point p7 = new Point(18.1, 0);
+    Point p8 = new Point(36.3, 3);
+    Assert.assertEquals(Point.distance(p7, p8), 18.445595680270124);
+
+  }
+}
