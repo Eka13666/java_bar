8904b8264cbe161e7336aea74a2a5dbbed3f82b5
eka <katrinmadness17@gmail.com>
2021-10-03 22:16:29 +0300

    Создание двухуровневой архитектуры тестового набора

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,42 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+  FirefoxDriver wd;
+
+
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+  private SessionHelper sessionHelper;
+
+  public void init() {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/index.php");
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
+  }
+
+  public void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+
+  public void stop() {
+    logout();
+    wd.quit();
+  }
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
+  }
+
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
+}
