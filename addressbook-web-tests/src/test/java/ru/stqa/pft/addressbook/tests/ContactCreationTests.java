3a2a23402ee6ec028a9016441939ec27bb04d14e
eka <katrinmadness17@gmail.com>
2021-11-07 18:12:39 +0300

    Вынеcено описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -42,7 +42,7 @@ public class ContactCreationTests extends TestBase {
     File photo = new File("src/test/resources/photo.jpeg");
     app.contact().create(contact);
     Contacts after = app.contact().all();
-    assertEquals(after.size(), before.size( ) + 1);
+    assertEquals(after.size(), before.size() + 1);
 
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
