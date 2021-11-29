9ac365e0bfbed9b8a04b24706b076cf245576dec
eka <katrinmadness17@gmail.com>
2021-11-29 20:19:22 +0300

    Доработан RegistrationTests + реализована передача файлов на удалённую машину по FTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -6,6 +6,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 
 public class TestBase {
 
@@ -13,12 +16,14 @@ public class TestBase {
           new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite(alwaysRun = true)
-  public void setUp() throws Exception {
+  public void setUp() throws IOException {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc_php", "config_inc_php.bak");
   }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown() {
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_inc_php.bak", "config_inc_php");
     app.stop();
   }
 }
